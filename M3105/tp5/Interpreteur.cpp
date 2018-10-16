08a8530fab5e14ef7604b0e7b169fc64ab4b2e67
cabezama <cabezama@pc-dg-spare1.iut2.upmf-grenoble.fr>
2018-10-16 16:24:39 +0200

    txt

--- M3105/tp5/Interpreteur.cpp
+++ M3105/tp5/Interpreteur.cpp
@@ -1,6 +1,7 @@
 #include "Interpreteur.h"
 #include <stdlib.h>
 #include <iostream>
+#include <vector>
 using namespace std;
 
 Interpreteur::Interpreteur(ifstream & fichier) :
@@ -144,18 +145,22 @@ Noeud* Interpreteur::instSiRiche() {
     Noeud* condition = expression();
     testerEtAvancer(")");
     Noeud* sequence = seqInst();
-    while(m_lecteur.getSymbole() == "sinonsi"){
-        testerEtAvancer("sinonsi");
-        testerEtAvancer("(");
-        Noeud* condition = expression();
-        testerEtAvancer(")");
-        Noeud* sequence = seqInst();
+    if(m_lecteur.getSymbole() == "sinonsi"){
+        vector<Noeud*> vectCondition;
+        while(m_lecteur.getSymbole() == "sinonsi"){        
+            testerEtAvancer("sinonsi");
+            testerEtAvancer("(");
+            vectCondition.push_back(expression());
+            testerEtAvancer(")");
+            Noeud* sequence = seqInst();
+        }
     }
+    
     if(m_lecteur.getSymbole() == "sinon"){
         testerEtAvancer("sinon");        
     }
     testerEtAvancer("finsi");
-    return nullptr;
     
+    return nullptr;    
 }
 
